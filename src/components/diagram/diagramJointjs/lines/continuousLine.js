846ff0d09a7eef60be999c4c04586462567b105d
miguelpaz418 <paz.luis@correounivalle.edu.co>
2022-01-22 19:53:48 -0500

    adding lines icons of panel

---
+++ src/components/diagram/diagramJointjs/lines/continuousLine.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt';
+
+export default function Icon(props) {
+
+  return (
+    <ArrowRightAltIcon style={{ fontSize: 28 }}/>
+  );
+}
